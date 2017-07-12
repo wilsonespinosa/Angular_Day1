da860931a8ae02cba24de70bdcf56722dd010d4f
Wilson Espinosa Bolivar <wilson.espinosa@yuxiglobal.com>
2017-07-12 08:46:33 -0500

    first task commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'my_WEBApplication';
+  author = 'Wilson Espinosa';
 }
